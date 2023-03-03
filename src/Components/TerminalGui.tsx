e17d12eec77f59dc42c18f6b8dcc426397259dea
Todd Grilliot <tbgrilliot@gmail.com>
2023-03-02 22:37:32 -0600

    got a block that types

--- src/Components/TerminalGui.tsx
+++ src/Components/TerminalGui.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import Block from './Block';
+// import Block from './Block';
 import Cursor from './Cursor';
-import Chalk from './Chalk';
+// import Chalk from './Chalk';
+
+import Block2 from './Block2';
 
 function TerminalGui() {
     return (
         <div className='p-24 bg-zinc-900 h-screen w-screen'>
-            <h1 className="text-white text-2xl font-bold underline">Hello world</h1>
-            <Block>Welcome to query terminal</Block>
+            <h1 className="text-white text-2xl font-bold underline">Hello <br/> world</h1>
+            {/* <Block>Welcome to query terminal</Block>
             <Block animated >
-                {/* <p className='text-white'>4 {' '}</p> */}
                 <Chalk v=' 1 2 3' />
                 <Chalk v={'1 2 3'} />
                 <Chalk v=" 3 " />
-            </Block>
-            {/* <div className='typing-demo'>
-                this is a typing demo
-            </div> */}
+            </Block> */}
+            <Block2 
+            //if both blocks have 'display: inline-block' then a div can display inline with another div.
+                lines={[
+                    {
+                        text: ['one ', 'two ', 'three words in a block ', 'fourth ', 'fifth'],
+                    },
+                    {
+                        text: ['Ape ', 'Bird ', 'Cat', ],
+                        styles: [{color: 'white'}, {color: 'red'}, {color: 'white'}]
+                    },
+                ]}
+            />
             <Cursor />
         </div>
     );
